0ae02eca9d86020faa55882ae70496137abb4538
Rob Brown <rib1356@hotmail.co.uk>
2017-11-06 01:14:28 +0000

    Added Angular4 and some communication

--- routes/api.js
+++ routes/api.js
@@ -5,6 +5,8 @@ const Product = require('../models/stock');
 const stockModel = require('../models/stock'); //Get db model to be able to find from
 
 
+//This will need to be changed to check for whos logged in so can check what they can see
+
 //Get all of the products from the DB
 router.get('/products',function(req, res, next){
   stockModel.find({}, function(err, db){
@@ -15,17 +17,22 @@ router.get('/products',function(req, res, next){
   });
 });
 
+//Will have to do some sort of token pass here?
+router.get('/:customerid',function(req, res, next){
+    //Get their current id and compare to check who they are then call another function
+});
+
 
 //Add another product to database
-router.post('/stock',function(req, res, next){
-  Product.create(req.body).then(function(product) { //Will create a new instance then save to db
+router.post('/products',function(req, res, next){
+  stockModel.create(req.body).then(function(product) { //Will create a new instance then save to db
     res.send(product);
 
   }).catch(next);
 });
 
 //Update a product in database (use this for changing price)
-router.put('/stock/:id',function(req, res, next){
+router.put('/products/:id',function(req, res, next){
   Product.findByIdAndUpdate({_id: req.params.id}, req.body).then(function(){
     Product.findOne({_id: req.params.id}).then(function(product){
       res.send(product);
@@ -34,7 +41,7 @@ router.put('/stock/:id',function(req, res, next){
 });
 
 //Delete a product from database Still doesnt work well but is it needed?
-router.delete('/stock/:id',function(req, res, next){
+router.delete('/products/:id',function(req, res, next){
   console.log(req.prarams.id);
   Product.findByIdAndRemove({_id:req.params.id}).then(function(product){
     res.send(product);
