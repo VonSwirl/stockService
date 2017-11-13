60261c99caaee52f6ce946b175334ddf519973da
Rob Brown <rib1356@hotmail.co.uk>
2017-11-13 20:30:35 +0000

    ChangedCode

--- routes/api.js
+++ routes/api.js
@@ -25,6 +25,29 @@ router.get('/products/:id',function(req, res, next){
       res.send(product);
     });
 });
+
+router.get('/productorder', function(req, res, next){
+
+  //Hi rob below is the order being put together
+  var order = [];
+  for(var prop in req.query){
+    var number = req.query[prop][1];
+
+    if(number > 0){
+      var ean = prop;
+      var avaliableStock = req.query[prop][0];
+      var enoughStock = (avaliableStock >= number) ? true : false;
+      var name = req.query[prop][2];
+      console.log('i should be adding ann order');
+      order.push({"ean" : ean, "enoughStock" : enoughStock, "number" : number , "name" : name});
+    }
+  }
+  //When you want to pass on the order the below object should be enough info :D
+  console.log("i should be ann order" , order);
+  console.log(req.query['HG1']);
+  return;
+});
+
 //var textValue = document.getElementById('textArea');
 //Will do a post when button is pressed on the products page
 router.post('/products', function(req, res, next){
@@ -32,15 +55,10 @@ router.post('/products', function(req, res, next){
   // res.render('productView', {textArea: req.body.textArea});
    //var test = req.body.textArea;
    if(req.body.checkbox = 'on'){
-     //console.log(req.body.textArea);
+     console.log(req.body.textArea);
    };
    console.log('button pressed');
-  // console.log(req.body.textArea);
-   //console.log(req.body.checkbox);
-//   console.log(textArea);
-// //  let stockModel = new stockModel();
-  //stockModel.title
-//  console.log(req.body.title);
+
   return;
 });
 
