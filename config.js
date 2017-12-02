0e30c2f43e5b401cce64ebb89734746cc7397298
Jerome Hurley <vonswirl@googlemail.com>
2017-12-02 20:36:01 +0000

    config file update to communicate across azure

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 var config = {}
 
-config.OrderServiceURL = 'http://localhost:3004/order/makeorder'
+//config.OrderServiceURL = 'http://localhost:3004/order/makeorder'
+config.OrderServiceURL = 'http://3amigoso.azurewebsites.net/order/makeorder'
 
 module.exports = config;
